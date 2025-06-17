5c6be564910388655bc2ce16fc516b9a354dc853
Sahil Sardhara <sardharasahil47@gmail.com>
2025-06-17 15:36:38 +0530

    days-8 create a model of catalog

---
+++ lib/models/catalog.dart
@@ -0,0 +1,28 @@
+class Item{
+
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image});
+
+}
+
+final product=[
+  Item(id: "01",
+      name: "iPhone 15 Pro Max",
+      desc: "Apple iPhone 15th generation",
+      price: 1299,
+      color: "#33505a",
+      image:"https://images.macrumors.com/t/OGS-wMpuHXbX6VkpJd6urJH1rEg=/1600x0/article-new/2023/09/iphone-15-pro-gray.jpg"
+  )
+
+];
