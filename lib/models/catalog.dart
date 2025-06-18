dd9b6838a0b2538e0dbab4238cef2df1b66c2bb8
Sahil Sardhara <sardharasahil47@gmail.com>
2025-06-18 11:07:26 +0530

    days-10 completed learn how to show items in home screen.

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,6 +1,23 @@
+class catalogmodel{
+  static final items=[
+    Item(id: 01,
+        name: "iPhone 15 Pro Max",
+        desc: "Apple iPhone 15th generation",
+        price: 1299,
+        color: "#33505a",
+        image:"https://images.macrumors.com/t/OGS-wMpuHXbX6VkpJd6urJH1rEg=/1600x0/article-new/2023/09/iphone-15-pro-gray.jpg"
+    )
+
+  ];
+
+}
+
+
+
+
 class Item{
 
-  final String id;
+  final num id;
   final String name;
   final String desc;
   final num price;
@@ -16,13 +33,3 @@ class Item{
 
 }
 
-final product=[
-  Item(id: "01",
-      name: "iPhone 15 Pro Max",
-      desc: "Apple iPhone 15th generation",
-      price: 1299,
-      color: "#33505a",
-      image:"https://images.macrumors.com/t/OGS-wMpuHXbX6VkpJd6urJH1rEg=/1600x0/article-new/2023/09/iphone-15-pro-gray.jpg"
-  )
-
-];
---
+++ lib/widget/item_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:untitled/models/catalog.dart';
+
+class ItemWidget extends StatelessWidget {
+  final Item item;
+  const ItemWidget({super.key, required this.item});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        onTap: (){
+          print("${item.name}is pressed");
+        },
+        leading: Image.network(item.image),
+        title: Text(item.name),
+        subtitle: Text(item.desc),
+        trailing: Text("\$${item.price}",
+        textScaleFactor: 1.5,
+        style: TextStyle(fontWeight: FontWeight.bold,
+        color: Colors.deepPurple
+        ),),
+      ),
+    );
+  }
+}
